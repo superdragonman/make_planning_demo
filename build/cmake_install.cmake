b560c27fe6f63df9efec943c8da65338fe2f190e
LiuZhaoyang <1343183241@qq.com>
2025-12-06 19:34:04 +0800

    second

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -37,6 +37,16 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/liuzhaoyang/C++Projects/make_planning_demo/build/src/pnc_map/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/liuzhaoyang/C++Projects/make_planning_demo/build/src/process/cmake_install.cmake")
+endif()
+
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
--- src/planning_main.cpp
+++ src/planning_main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "include/process.h"
+#include "process.h"
 
 int main()
 {
